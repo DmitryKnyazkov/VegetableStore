744180d6ecb6634caa1be8a786a0b177dd642457
DmitryKnyazkov <kde.2015@yandex.ru>
2023-06-14 11:05:07 +0300

    initian commit2

--- store/urls.py
+++ store/urls.py
@@ -4,8 +4,9 @@ from django.urls import path, include
 
 from .views import IndexViewProduct, IndexViewShop, IndexViewStoreCart
 
+app_name = 'store'
 
 urlpatterns = [
-    path('product/', IndexViewProduct.as_view()),
-    path('', IndexViewShop.as_view()),
-    path('cart/', IndexViewStoreCart.as_view()),]
+    path('', IndexViewShop.as_view(), name='shop'),
+    path('cart/', IndexViewStoreCart.as_view(), name='cart'),
+    path('product/<int:id>/', IndexViewProduct.as_view(),name='product')]
